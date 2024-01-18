019a3582d9a959aa5f7d3cf0e5f9db5624693702
jasper.lai <jasper.lai@gmail.com>
2024-01-18 14:45:48 +0800

    加入 IRandomGenerator 介面 及 RandomGenerator 類別

---
+++ ASPNetCore6Random/Wrapper/IRandomGenerator.cs
@@ -0,0 +1,10 @@
+﻿namespace ASPNetCore6Random.Wrapper
+{
+    public interface IRandomGenerator
+    {
+        int Next();
+        int Next(int maxValue);
+        int Next(int minValue, int maxValue);
+        double NextDouble();
+    }
+}
